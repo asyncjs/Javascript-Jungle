f1e6388489a5603381fb3843bcaf56dfbdf876cf
Simon Elliott <simon@papercreatures.com>
2011-09-07 17:23:56 +0100

    grass sticks to bottom

--- javascript/grass.js
+++ javascript/grass.js
@@ -3,15 +3,18 @@ jj.createLayer('grass', function (layer) {
   var el = layer.el;
   var jq = jj.jQuery;
   // Set the size of your layer.
-  jq(layer).size({width: '100%', height: '100%'});
-  var rp = Raphael(el[0],layer.size().width,layer.size().height);
-  var path = ["M 0 100"];
+  jq(layer).size({ width: '100%', height: '100%'});
+  var w = layer.size().width, 
+      h = layer.size().height;
+  var rp = Raphael(el[0],w,h);
+  var path = ["M 0 " + (h)];
   //create the grass path
-  for (var i = 0; i <= layer.size().width; i+=30){
-    path.push("L " + i + " 0");
-    path.push("L " + i + " 100");
+  for (var i = 0; i <= layer.size().width; i+=20){
+    path.push("L " + i + " " + (h - 70));
+    path.push("L " + i + " " + h);
   };
   path.push("z");
+  console.log(path.join(" "));
   var grass = rp.path(path.join(" ")).attr({
     stroke: '100-#0f5b0f-#37cf37',
     fill: '100-#0f5b0f-#37cf37'
