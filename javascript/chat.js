e028169b361618f25c5fc1399d45563952a5cc3b
Premasagar Rose <p@dharmafly.com>
2011-09-07 10:40:08 -0400

    chat stub

---
+++ javascript/chat.js
@@ -0,0 +1,38 @@
+/*jj.createLayer('creature', function (layer) {
+  // the div element for the layer.
+  var element = layer.el;
+
+  // Set the size of your layer.
+  layer.size({width: 50, height: 50});
+
+  // Set/Get the size of the layer.
+  layer.size() // {width: 50, height: 50}
+  // Set/Get the position of the layer.
+  layer.position() // {top: 50, left: 50, zindex: 0}
+  // Set/Get any metadata.
+  layer.data({heelo: 'world'});
+
+  // Trigger an event on your creature.
+  layer.trigger('myevent');
+
+  // Get a read only version of another layer.
+  var prem = jj.get('prem');
+
+  // Bind an event on Prems creature.
+  prem.bind('prems event');
+
+  // Returns read-only copies of all creatures.
+  jj.all()
+
+  // Trigger a global event.
+  jj.trigger('hello');
+
+  // Bind to a global event.
+  jj.bind('tick', function (frame) {
+    // Animate here.
+  });
+
+  jj.bind('time', function (hours, seconds) {
+
+  });
+});*/
