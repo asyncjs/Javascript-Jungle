626d15a5b94d7112be34bc3fb3e89021600cbcf3
Simon Elliott <simon@papercreatures.com>
2011-09-07 17:17:05 +0100

    merge

--- javascript/grass.js
+++ javascript/grass.js
@@ -1,10 +1,19 @@
 jj.createLayer('grass', function (layer) {
   // the div element for the layer.
   var el = layer.el;
-
+  var jq = jj.jQuery;
   // Set the size of your layer.
-  jj.jQuery(layer).size({width: 50, height: 100});
-
-  var paper = Raphael(el[0],50,100);
-  paper.circle(25, 50, 25);
+  jq(layer).size({width: '100%', height: '100%'});
+  var rp = Raphael(el[0],layer.size().width,layer.size().height);
+  var path = ["M 0 100"];
+  //create the grass path
+  for (var i = 0; i <= layer.size().width; i+=30){
+    path.push("L " + i + " 0");
+    path.push("L " + i + " 100");
+  };
+  path.push("z");
+  var grass = rp.path(path.join(" ")).attr({
+    stroke: '100-#0f5b0f-#37cf37',
+    fill: '100-#0f5b0f-#37cf37'
+  });
 });
