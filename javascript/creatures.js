a09050f32d12d02ad4ba70c15c3f63121776d1d6
Aron Carroll <self@aroncarroll.com>
2011-09-08 22:10:50 +0100

    Added happydog.js

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -3,6 +3,7 @@ if (window.location.search.indexOf('live') > -1) {
     'https://raw.github.com/gist/1204436/',
     'https://raw.github.com/gist/1204505/',
     'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
-    'https://raw.github.com/gist/1204683/'  //happygiraffe.js
+    'https://raw.github.com/gist/1204683/', // happygiraffe.js,
+    'https://raw.github.com/gist/1204698/'  // happydog.js
   );
 }
