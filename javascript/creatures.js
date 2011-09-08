901bd4d5b4aa444dac8e1776e7052c435548699b
Aron Carroll <self@aroncarroll.com>
2011-09-08 21:04:45 +0100

    Added "sun"

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -1,5 +1,6 @@
 if (window.location.search.indexOf('live') > -1) {
   jj.load(
-    'https://raw.github.com/gist/1204436/951377bbc3f872e19872f195fd1cee8f01b0096c/triangula.js'
+    'https://raw.github.com/gist/1204436/951377bbc3f872e19872f195fd1cee8f01b0096c/triangula.js',
+    'https://raw.github.com/gist/1204505/11b864f2bc2fdcf9601d01f73c1f9879d8a7bba0/sun-Hugheth.js'
   );
 }
