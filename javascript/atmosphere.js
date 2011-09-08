ea6a6d62d7af47323bbd1544c1d0be0e05b14f37
Simon Elliott <simon@papercreatures.com>
2011-09-08 17:40:42 +0100

    sky changes every 10 min

--- javascript/atmosphere.js
+++ javascript/atmosphere.js
@@ -2,15 +2,13 @@ jj.createCreature('atmosphere', function (layer) {
   // we don't need layer for this as it's provided.
   layer.el.remove();
   var el = jj.jQuery('div#jungle');
-  var lhr = 0;
   jj.bind('clock', function(hr,m) {
-    if(lhr != hr) {
+    if(m%10===0) {
       var tar = 255 - Math.floor(Math.abs(hr-12) * 21);
       //these need padding but it has a useful side effect
       var b = tar.toString(16),
           r = (Math.floor(tar/2)).toString(16);
       el.css('backgroundColor', '#00' + r + b);
-      lhr = hr;
     }
   });
 });
