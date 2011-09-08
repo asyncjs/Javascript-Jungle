d2883ecde68e5a05ab3cdd80c22bbcdc854bfa4a
Aron Carroll <self@aroncarroll.com>
2011-09-08 22:00:43 +0100

    Added butterfly_colmjude.js and happygiraffe.js

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -1,6 +1,8 @@
 if (window.location.search.indexOf('live') > -1) {
   jj.load(
     'https://raw.github.com/gist/1204436/',
-    'https://raw.github.com/gist/1204505/'
+    'https://raw.github.com/gist/1204505/',
+    'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
+    'https://raw.github.com/gist/1204683/'  //happygiraffe.js
   );
 }
