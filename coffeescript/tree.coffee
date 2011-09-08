ca81f30bded0575556de8dc9eeef397896c0d60a
Simon Elliott <simon@papercreatures.com>
2011-09-08 17:38:11 +0100

    trees

---
+++ coffeescript/tree.coffee
@@ -0,0 +1,41 @@
+class Tree
+  constructor: (canvas, @x,@y) ->
+    @context = canvas.getContext '2d'
+    @context.strokeStyle = @color
+    @_makeBranch @x, @y, @branch_length, -Math.PI/2, @max_size
+  x: 100
+  y: 400
+  max_sub_branch: 5
+  max_sub_angle: 3*Math.PI/4
+  max_size: 6
+  branch_length: 200
+  color: "#754121"
+  _makeBranch: (start_x, start_y, length, angle, size) ->
+    if size > 0
+      @context.lineWidth = size*2
+      @context.beginPath()
+      @context.moveTo start_x, start_y
+      end_x = start_x + length * Math.cos(angle)
+      end_y = start_y + length * Math.sin(angle)
+
+      @context.lineTo end_x, end_y
+      @context.stroke();
+
+      sub_branch = Math.floor Math.random() * (@max_sub_branch - 1) + 1
+      branch_length_dimin = .5 + Math.random()/2
+      for i in [0 .. sub_branch]
+        newLength = length * branch_length_dimin
+        newAngle = angle + Math.random() * @max_sub_angle - @max_sub_angle / 2
+        newSize = size - 1
+        @_makeBranch end_x, end_y, newLength, newAngle, newSize
+      @context.closePath();
+
+jj.createCreature 'trees', (layer) ->
+  layer.size width: '100%', height: '100%'
+  canvas = document.createElement 'canvas'
+  layer.el.append canvas
+  canvas.width  = layer.size().width
+  canvas.height = layer.size().height
+  new Tree canvas, 100, canvas.height
+  new Tree canvas, canvas.width - 100, canvas.height
+  new Tree canvas, canvas.width / 2, canvas.height
--- javascript/tree.js
+++ javascript/tree.js
@@ -1,10 +1,55 @@
-jj.createCreature('tree', function (layer) {
-  // the div element for the layer.
-  var el = layer.el;
-  // Set the size of your layer.
-  jQuery(layer).size({width: 50, height: 100});
-  var rp = Raphael(el[0],50,100);
-  var tree = rp.path("M10 10L90 90").attr({
-    stroke: 'brown'
+(function() {
+  var Tree;
+  Tree = (function() {
+    function Tree(canvas, x, y) {
+      this.x = x;
+      this.y = y;
+      this.context = canvas.getContext('2d');
+      this.context.strokeStyle = this.color;
+      this._makeBranch(this.x, this.y, this.branch_length, -Math.PI / 2, this.max_size);
+    }
+    Tree.prototype.x = 100;
+    Tree.prototype.y = 400;
+    Tree.prototype.max_sub_branch = 5;
+    Tree.prototype.max_sub_angle = 3 * Math.PI / 4;
+    Tree.prototype.max_size = 6;
+    Tree.prototype.branch_length = 200;
+    Tree.prototype.color = "#754121";
+    Tree.prototype._makeBranch = function(start_x, start_y, length, angle, size) {
+      var branch_length_dimin, end_x, end_y, i, newAngle, newLength, newSize, sub_branch;
+      if (size > 0) {
+        this.context.lineWidth = size * 2;
+        this.context.beginPath();
+        this.context.moveTo(start_x, start_y);
+        end_x = start_x + length * Math.cos(angle);
+        end_y = start_y + length * Math.sin(angle);
+        this.context.lineTo(end_x, end_y);
+        this.context.stroke();
+        sub_branch = Math.floor(Math.random() * (this.max_sub_branch - 1) + 1);
+        branch_length_dimin = .5 + Math.random() / 2;
+        for (i = 0; 0 <= sub_branch ? i <= sub_branch : i >= sub_branch; 0 <= sub_branch ? i++ : i--) {
+          newLength = length * branch_length_dimin;
+          newAngle = angle + Math.random() * this.max_sub_angle - this.max_sub_angle / 2;
+          newSize = size - 1;
+          this._makeBranch(end_x, end_y, newLength, newAngle, newSize);
+        }
+        return this.context.closePath();
+      }
+    };
+    return Tree;
+  })();
+  jj.createCreature('trees', function(layer) {
+    var canvas;
+    layer.size({
+      width: '100%',
+      height: '100%'
+    });
+    canvas = document.createElement('canvas');
+    layer.el.append(canvas);
+    canvas.width = layer.size().width;
+    canvas.height = layer.size().height;
+    new Tree(canvas, 100, canvas.height);
+    new Tree(canvas, canvas.width - 100, canvas.height);
+    return new Tree(canvas, canvas.width / 2, canvas.height);
   });
-});
+}).call(this);
