823f45acb4f9c3f6bc75c557cda1d714bc612336
Simon Elliott <simon@papercreatures.com>
2011-09-08 15:19:27 +0100

    more grass events + more stage like

--- javascript/grass.js
+++ javascript/grass.js
@@ -6,28 +6,61 @@ jj.createCreature('grass', function (layer) {
   layer.size({ width: '100%', height: '100%'});
   var w = layer.size().width, 
       h = layer.size().height;
-  var rp = Raphael(el[0],w,h);
   var curGrowth = 0;
-  var gen_path = function(growth) {
-    var path = ["M 0 " + h];
-    for (var i = 0; i <= layer.size().width; i+=20){
+  var gen_path = function(growth,os) {
+    os || (os = 0);
+    var path = [['M',os,h].join(' ')];
+    for (var i = os; i <= w; i+=20){
       //this can't be random every time.
+      //edges grow higher
       var gh = Math.floor(Math.random()*100) + 70;
-      path.push("L " + i + " " + (h - growth - gh ));
-      path.push("L " + i + " " + (h + gh));
+      var eos = Math.abs(i - (w/2));
+      if (eos > w/3) gh += parseInt(eos - w/3,10);
+      path.push(['L',i,(h - growth - gh )].join(' '));
+      path.push(['L',i,(h + gh)].join(' '));
     };
-    path.push("z");
-    return path.join(" ");
+    return path.concat('z').join(" ");
   };
   //create the grass path
-  console.log(gen_path(curGrowth));
+  //grass should be a class.
+  //double layer it
+
+  var rp = Raphael(el[0],w,h);
   var grass = rp.path(gen_path(curGrowth)).attr({
     stroke: '100-#0f5b0f-#37cf37',
     fill: '100-#0f5b0f-#37cf37'
   });
 
+  var grass2 = rp.path(gen_path(curGrowth-60,10)).attr({
+    stroke: '100-#10360C-#15610F',
+    fill: '100-#10360C-#15610F'
+  });
+
+
   jj.bind('breakfast',function() {
     curGrowth += 20;
     grass.animate({path: gen_path(curGrowth)}, 1000);
+    grass2.animate({path: gen_path(curGrowth-60,10)}, 1000);
   });
+
+  jj.bind('rain',function() {
+    curGrowth += 50;
+    grass.animate({path: gen_path(curGrowth)}, 1000);
+    grass2.animate({path: gen_path(curGrowth-60,10)}, 1000);
+  });
+
+  jj.bind('eaten',function() {
+    if(curGrowth>20) {
+      curGrowth -= 20;
+      grass.animate({path: gen_path(curGrowth)}, 1000);
+      grass2.animate({path: gen_path(curGrowth-60,10)}, 1000);
+      return 20;
+    }
+    //provide food amount as return
+    return 0;
+  }); 
+  
 });
+
+
+//todo edge growth, grow on rain, shrink on eat
