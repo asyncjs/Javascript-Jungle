42e5647a40bc37b70c2914021843f7a023442f4b
Simon Elliott <simon@papercreatures.com>
2011-09-08 15:37:31 +0100

    stars at night

---
+++ javascript/stars.js
@@ -0,0 +1,24 @@
+  jj.createCreature('stars', function (layer) {
+    // the div element for the layer.
+    var el = layer.el;
+    var jq = jj.jQuery;
+    layer.size({ width: '100%', height: '30%'});
+    var w = layer.size().width, 
+        h = layer.size().height;
+    var rp = Raphael(el[0],w,h);
+    var stars = [];
+    jj.bind('clock', function(hr,m) {
+      if((hr>21 || hr<7) && m%2) {
+        stars.push(
+          rp.circle( 
+            Math.floor(Math.random()*w),
+            Math.floor(Math.random()*h),
+            hr>23 || hr <1 ? 2 : 1
+          ).attr({
+            stroke:'#fff',
+            fill: '#fff'
+          })
+        );
+      }
+    });
+  });
