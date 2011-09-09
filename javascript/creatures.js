b2c1f8d51d26aa100e0d6cfab29f8bf162694e77
Jim Purbrick <jim@jimpurbrick.com>
2011-09-09 13:41:19 +0100

    Fixed extra comma.

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -7,6 +7,6 @@ if (window.location.search.indexOf('live') > -1) {
     'https://raw.github.com/gist/1204698/', // happydog.js
     'https://raw.github.com/gist/1204788/',  // birds.js
     'https://raw.github.com/gist/1204932/', // toucan @rakugojon
-    'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
+    'https://raw.github.com/gist/1206090/' // ufo @JimPurbrick
   );
 }
