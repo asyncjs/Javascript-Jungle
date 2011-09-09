a1c9b9b234f337ded7523e33cfd45a8063e60e50
Jim Purbrick <jim@jimpurbrick.com>
2011-09-09 13:38:22 +0100

    Added ufo gist to creatures.js

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -6,6 +6,7 @@ if (window.location.search.indexOf('live') > -1) {
     'https://raw.github.com/gist/1204683/', // happygiraffe.js,
     'https://raw.github.com/gist/1204698/', // happydog.js
     'https://raw.github.com/gist/1204788/',  // birds.js
-    'https://raw.github.com/gist/1204932' // toucan @rakugojon
+    'https://raw.github.com/gist/1204932/', // toucan @rakugojon
+    'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
   );
 }
