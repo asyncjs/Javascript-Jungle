4071f00ac90af9a0d100d7e5102d5d671e7c8179
Premasagar <p@dharmafly.com>
2011-09-10 10:05:45 -0400

    new chat interface

--- javascript/chat.js
+++ javascript/chat.js
@@ -8,29 +8,25 @@ jj.createCreature("chat", function (creature) {
         styleElem = jQuery("<style/>").appendTo("head");
 
     creature
-        .size({width:"220px", height:"100%"})
+        .size({width:220, height:"100%"})
         .position({right:0, top:0, zIndex:999999})
-        .data({background: true});
+        .data({background:true});
     
     styleElem.text(
-        ".chat {list-style-type:none; padding:0.5em 1em; margin:0; font-family:monospace; overflow:auto; height:100%; opacity:0.7; color:#fff; background-color:#000; }" +
-        ".chat .chat-entry{ margin:0.38em 0; }" +
-        ".chat .chat-name{ color:#3f3; }" +
-        ".chat .chat-delimiter{ color:#777; }" +
-        ".chat .chat-message{ color:#ff9; font-weight:bold; }"
+        ".creature ul.chat {list-style-type:none; padding:0.5em 1em; margin:0; font-family:monospace; overflow:auto; height:100%; opacity:0.7; color:#fff; background-color:#000; }" +
+        ".creature ul.chat .chat-entry{ margin:0.38em 0; }" +
+        ".creature ul.chat .chat-name{ color:#3f3; }" +
+        ".creature ul.chat .chat-delimiter{ color:#777; }" +
+        ".creature ul.chat .chat-message{ color:#ff9; font-weight:bold; }"
     );
     
-    creature.log = function(eventName, message){
-        var nameElem, delimElem, messageElem, report;
-    
-        /*
-        if (eventName === "tick" || eventName === "clock"){
-            return;
-        }
-        */
+    function log(message, creature){
+        var creatureName = creature.name ? 
+                creature.name() : creature,
+            nameElem, delimElem, messageElem, report;
         
         report = jQuery("<li class='chat-entry'></li>");
-        nameElem = jQuery("<span class='chat-name'></span>").html(eventName);
+        nameElem = jQuery("<span class='chat-name'></span>").text(creatureName || "Anon");
         
         if (message){
             delimElem = jQuery("<span class='chat-delimiter'>: </span>");
@@ -41,8 +37,11 @@ jj.createCreature("chat", function (creature) {
         logElem[0].scrollTop = logElem.height();
     };
     
-    creature.log("asyncjs", "Welcome to the JavaScript Jungle");
-    creature.bind("log", creature.log);
+    // jj.get("chat").trigger("log", "foo", creature);
+    creature.bind("log", log);
+    
+    // jj.chat("foo", creature);
+    jj.chat = log;
 
-    jj.bind("all", creature.log);
+    jj.bind("all", log);
 });
