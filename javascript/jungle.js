253065abb881b2378fb038f7c5c591dbcb25c7f4
Premasagar <p@dharmafly.com>
2011-09-10 17:26:46 -0400

    jungle.js: Creature.center() method

--- javascript/jungle.js
+++ javascript/jungle.js
@@ -208,6 +208,16 @@
       this.el.css(position);
       return this;
     },
+    
+    // Centers the creature in the world.
+    center: function () {
+      var worldCenter = jj.center();
+      this.position({
+        left: worldCenter.left - (this.width()  / 2),
+        top:  worldCenter.top  - (this.height() / 2) 
+      });
+      return this;
+    },
 
     // Returns a readonly version of the creature. None of the setters will
     // have any effect.
