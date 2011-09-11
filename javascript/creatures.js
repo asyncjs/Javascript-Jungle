dceedba2f82bd58785bbbfb6891f00e32c7745c6
Simon Elliott <simon@papercreatures.com>
2011-09-11 04:30:39 +0100

    new creature

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -5,8 +5,9 @@ if (window.location.search.indexOf('live') > -1) {
     'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
     'https://raw.github.com/gist/1204683/', // happygiraffe.js,
     'https://raw.github.com/gist/1204698/', // happydog.js
-    'https://raw.github.com/gist/1204788/',  // birds.js
+    'https://raw.github.com/gist/1204788/', // birds.js
     'https://raw.github.com/gist/1204932/', // toucan @rakugojon
-    'https://raw.github.com/gist/1206090/' // ufo @JimPurbrick
+    'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
+    'https://raw.github.com/gist/1208781/'  //rakugojon
   );
 }
