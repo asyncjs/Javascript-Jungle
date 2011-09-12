3d9b8ea259b1e81bce6cbdb0fc69295bc0b3b07d
Premasagar Rose <p@dharmafly.com>
2011-09-12 13:30:18 -0400

    all creature js files are now in creatures.js

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -5,17 +5,28 @@
         return (new RegExp(pattern)).test(window.location.search);
     }
 
+    // Standard creatures
+    jj.load(
+        "javascript/atmosphere.js",
+        "javascript/chat.js",
+        "javascript/clock.js",
+        "javascript/stars.js",
+        "javascript/tree.js",
+        "javascript/grass.js"
+    );
+
+    // Creatures from the community
     if (!hasSearchParam("dev")){
       jj.load(
-        'https://raw.github.com/gist/1204436/',
-        'https://raw.github.com/gist/1204505/',
-        'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
-        'https://raw.github.com/gist/1204683/', // happygiraffe.js,
-        'https://raw.github.com/gist/1204698/', // happydog.js
-        'https://raw.github.com/gist/1204788/', // birds.js
-        'https://raw.github.com/gist/1204932/', // toucan @rakugojon
-        'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
-        'https://raw.github.com/gist/1208781/'  //rakugojon
+        "https://raw.github.com/gist/1204436/", // triangula
+        "https://raw.github.com/gist/1204505/", // sun
+        "https://raw.github.com/gist/1204620/", // butterfly_colmjude.js
+        "https://raw.github.com/gist/1204683/", // happygiraffe-snowman
+        "https://raw.github.com/gist/1204698/", // happydog
+        "https://raw.github.com/gist/1204788/", // flocking birds (almost-boid)
+        "https://raw.github.com/gist/1204932/", // toucan @rakugojon
+        "https://raw.github.com/gist/1206090/", // ufo @JimPurbrick
+        "https://raw.github.com/gist/1208781/"  // angrybird @rakugojon
       );
     }
 }());
