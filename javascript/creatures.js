57d8ea27b591dc9ce5f6662c1275b10c826c7871
Premasagar Rose <p@dharmafly.com>
2011-09-12 06:12:12 -0400

    now loading creatures by default, with no need to add '?live' to the URL; for development, add '?dev' to remove all creatures

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -1,13 +1,21 @@
-if (window.location.search.indexOf('live') > -1) {
-  jj.load(
-    'https://raw.github.com/gist/1204436/',
-    'https://raw.github.com/gist/1204505/',
-    'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
-    'https://raw.github.com/gist/1204683/', // happygiraffe.js,
-    'https://raw.github.com/gist/1204698/', // happydog.js
-    'https://raw.github.com/gist/1204788/', // birds.js
-    'https://raw.github.com/gist/1204932/', // toucan @rakugojon
-    'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
-    'https://raw.github.com/gist/1208781/'  //rakugojon
-  );
-}
+(function(){
+    function hasSearchParam(param, value){
+        var pattern = "^[\?&]" + param + 
+            (typeof value === "undefined" ? "($|[=&])" : "=" + value + "($|&)");
+        return (new RegExp(pattern)).test(window.location.search);
+    }
+
+    if (!hasSearchParam("dev")){
+      jj.load(
+        'https://raw.github.com/gist/1204436/',
+        'https://raw.github.com/gist/1204505/',
+        'https://raw.github.com/gist/1204620/', // butterfly_colmjude.js
+        'https://raw.github.com/gist/1204683/', // happygiraffe.js,
+        'https://raw.github.com/gist/1204698/', // happydog.js
+        'https://raw.github.com/gist/1204788/', // birds.js
+        'https://raw.github.com/gist/1204932/', // toucan @rakugojon
+        'https://raw.github.com/gist/1206090/', // ufo @JimPurbrick
+        'https://raw.github.com/gist/1208781/'  //rakugojon
+      );
+    }
+}());
