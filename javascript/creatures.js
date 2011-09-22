ec4acc87217dcc790e319309abdea3b207537c28
Premasagar Rose <p@dharmafly.com>
2011-09-22 14:55:23 -0400

    added placeholder for new creature scripts

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -30,8 +30,11 @@
                 "https://raw.github.com/gist/1208781/", // angrybird @rakugojon
                 //"creatures/_lion/_lion.js", 				// pete goodman - @thegingerbloke
                 //"creatures/pavilion-crab/pavilion-crab.js"	// joseph steiner - 
-				"https://raw.github.com/gist/1218733/", // _lion
-				"https://raw.github.com/gist/0162a62a2c61f5fd74f4/" // pavilion crab
+                "https://raw.github.com/gist/1218733/", // _lion
+                "https://raw.github.com/gist/0162a62a2c61f5fd74f4/" // pavilion crab
+				
+				// * PUT YOUR DEV CREATURE SCRIPT HERE *
+				// "creatures/mycreature.js"
               );
             }
         },
