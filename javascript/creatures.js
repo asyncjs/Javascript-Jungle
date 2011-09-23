f9e78a85de7d6308602b6dcb87bb9b80b80118f6
Pete Goodman <pete@petegoodman.com>
2011-09-23 07:47:16 +0100

    Updating Buzzy fly with Spike's latest updates

--- javascript/creatures.js
+++ javascript/creatures.js
@@ -19,20 +19,20 @@
             // Creatures from the community
             if (!hasSearchParam("dev")){
               jj.load(
-                "https://raw.github.com/gist/1204436/", // triangula
-                "https://raw.github.com/gist/1204505/", // sun
-                "https://raw.github.com/gist/1204620/", // butterfly_colmjude.js
-                "https://raw.github.com/gist/1204683/", // happygiraffe-snowman
-                "./creatures/happydog/happydog.js", // happydog
-                "./creatures/herm-boids/herm-boids.js", // @almostobsolete - flocking birds (herm-boid)
-                "https://raw.github.com/gist/1204932/", // toucan @rakugojon
-                "https://raw.github.com/gist/1206090/", // ufo @JimPurbrick
-                "https://raw.github.com/gist/1208781/", // angrybird @rakugojon
-                "creatures/lion/lion.js",               // pete goodman - @thegingerbloke
+                "https://raw.github.com/gist/1204436/",     // triangula
+                "https://raw.github.com/gist/1204505/",     // sun
+                "https://raw.github.com/gist/1204620/",     // butterfly_colmjude.js
+                "https://raw.github.com/gist/1204683/",     // happygiraffe-snowman
+                "./creatures/happydog/happydog.js",         // happydog
+                "./creatures/herm-boids/herm-boids.js",     // @almostobsolete - flocking birds (herm-boid)
+                "https://raw.github.com/gist/1204932/",     // toucan @rakugojon
+                "https://raw.github.com/gist/1206090/",     // ufo @JimPurbrick
+                "https://raw.github.com/gist/1208781/",     // angrybird @rakugojon
+                "creatures/lion/lion.js",                   // pete goodman - @thegingerbloke
                 "creatures/pavilion-crab/pavilion-crab.js", // joseph steiner - 
-                "creatures/hippoBee/hippoBee.js",       // riccardo - marco 
-                "creatures/elephant/elephant.js",        // piotr rochala - @rochal
-                "creatures/buzzy-fly/buzzy-fly.js"      // Fly - Spike & Shanee
+                "creatures/hippoBee/hippoBee.js",           // riccardo - marco 
+                "creatures/elephant/elephant.js",           // piotr rochala - @rochal
+                "creatures/buzzy-fly/buzzy-fly.js"          // Fly - Spike & Shanee
                 
                 // * PUT YOUR DEV CREATURE SCRIPT HERE *
                 // "creatures/mycreature.js"
